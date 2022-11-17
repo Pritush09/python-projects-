81cb86fb37b9e2d5537bc5ffef8e4515a75bec1e
Pritush Singh <mynameispritush.15@gmail.com>
2022-11-17 22:16:07 +0530

    done with the encrption anfd decryption of the messages

--- Substitution Cipher encryption.py
+++ Substitution Cipher encryption.py
@@ -5,7 +5,7 @@ chars = " " + string.punctuation + string.digits + string.ascii_letters
 # creating the set of the encrypted letter from which we will use the letters of the given message
 
 chars = list(chars)
-key = chars.copy()
+key = chars.copy() #This for the comparing with the chars so the given charater in char is replaced by the same character in key and vice versa
 
 random.shuffle(key)
 
@@ -27,6 +27,7 @@ for letter in plain_text:
 print(cipher_text)
 plain = ''
 encrypted_text = input("Enter the encrypted text : ")
+# this for the decryption of the message
 for letter in cipher_text:
     index_ = key.index(letter)
     plain += chars[index_]
